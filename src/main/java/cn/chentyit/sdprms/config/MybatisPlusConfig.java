e95da0cc886d17f836c34f64d889590a2f8c6f98
chentyit <chentyit@qq.com>
2020-04-15 11:06:26 +0800

    【软件缺陷预测论文仓库】后台管理系统 —— 修改单例模式为多例，防止 BaseMapper 单例被复用造成 sql 逻辑紊乱

--- src/main/java/cn/chentyit/sdprms/config/MybatisPlusConfig.java
+++ src/main/java/cn/chentyit/sdprms/config/MybatisPlusConfig.java
@@ -8,8 +8,10 @@ import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.service.additional.update.impl.LambdaUpdateChainWrapper;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import javax.annotation.Resource;
 
@@ -37,26 +39,31 @@ public class MybatisPlusConfig<T> {
     private ThesisScholarMapper thesisScholarMapper;
 
     @Bean
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public QueryWrapper<T> getWrapper() {
         return new QueryWrapper<>();
     }
 
     @Bean
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public LambdaQueryWrapper<T> getLambdaQuery() {
         return new LambdaQueryWrapper<>();
     }
 
     @Bean
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public UpdateWrapper<T> getUpdateWrapper() {
         return new UpdateWrapper<>();
     }
 
     @Bean
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public LambdaUpdateWrapper<T> getLambdaUpdateWrapper() {
         return Wrappers.lambdaUpdate();
     }
 
     @Bean
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
