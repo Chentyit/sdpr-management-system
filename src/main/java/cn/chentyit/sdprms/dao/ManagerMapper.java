9f6084adbf86a942fa92a9b69f17d3b21880ac64
chentyit <chentyit@qq.com>
2020-04-15 17:37:32 +0800

    【软件缺陷预测论文仓库】后台管理系统 —— 修改代码 BUG

--- src/main/java/cn/chentyit/sdprms/dao/ManagerMapper.java
+++ src/main/java/cn/chentyit/sdprms/dao/ManagerMapper.java
@@ -3,7 +3,6 @@ package cn.chentyit.sdprms.dao;
 import cn.chentyit.sdprms.model.entity.Manager;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 /**
  * @Author Chentyit
