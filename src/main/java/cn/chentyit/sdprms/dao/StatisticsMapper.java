e1458b338be6e274c2cfe6ad62a07091294d38b1
chentyit <chentyit@qq.com>
2020-04-16 09:10:06 +0800

    【软件缺陷预测论文仓库】后台管理系统 —— 修改自定义 Sql 语句

--- src/main/java/cn/chentyit/sdprms/dao/StatisticsMapper.java
+++ src/main/java/cn/chentyit/sdprms/dao/StatisticsMapper.java
@@ -2,6 +2,7 @@ package cn.chentyit.sdprms.dao;
 
 import cn.chentyit.sdprms.model.pojo.NumberOfPublication;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -15,7 +16,8 @@ public interface StatisticsMapper {
 
     /**
      * 获取每年各个主题数数量
+     * @param year 10 年前的年份
      * @return
      */
-    List<NumberOfPublication> getNumberOfPublication();
+    List<NumberOfPublication> getNumberOfPublication(@Param("year") Integer year);
 }
