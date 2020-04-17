eb750c931ecc3afe3a040a26a6ff9ee6072fe505
chentyit <chentyit@qq.com>
2020-04-17 10:17:37 +0800

    【软件缺陷预测论文仓库】后台管理系统 —— 完成单项删除数据

--- src/main/resources/static/js/management/thesis.js
+++ src/main/resources/static/js/management/thesis.js
@@ -1,27 +1,51 @@
-$(".thesis-delete-custom").click(function () {
-    // 获取到表格信息
-    const l = $("tr").filter(".selected");
+$(function () {
+    // 点击条目 delete 删除
+    $(document).on("click", ".thesis-delete-btn-custom", function () {
+        let thesisId = $(this).attr("id")
 
-    // 获取 Id 集合
-    let ids = []
-    for (let i = 0; i < l.length; i++) {
-        ids.push(parseInt(l[i].cells.item(0).innerText));
-    }
-
-    // 将要删除的信息 Id 发送给后端
-    $.ajax({
-        "type": "post",
-        "dataType": "text",
-        "contentType": "application/json",
-        "async": false,
-        "url": "/thesis/delete",
-        "data": JSON.stringify(ids),
-        "success": function (data) {
-            console.log("success")
-            window.location.href = "/thesis"
-        },
-        "error": function (data) {
-            console.log("failure")
-        }
+        // 将 Id 值传递给弹窗
+        let tar = $(".modal-body p")
+        tar.text("||" + thesisId + "|| 数据条目即将删除，是否确认操作!!!")
     })
+
+    $(document).on("click", ".thesis-delete-one-custom", function () {
+        let item = $(".modal-body p").text()
+
+        let ids = [parseInt(item.split("||")[1].split("-")[1])]
+
+        deleteByIds(ids)
+    })
+
+    // 多项选中删除
+    $(document).on("click", ".thesis-delete-selected-custom", function () {
+        // 获取到表格信息
+        const l = $("tr").filter(".selected");
+
+        // 获取 Id 集合
+        let ids = []
+        for (let i = 0; i < l.length; i++) {
+            ids.push(parseInt(l[i].cells.item(0).innerText));
+        }
+
+        deleteByIds(ids)
+    })
+
+    function deleteByIds(ids) {
+        // 将要删除的信息 Id 发送给后端
+        $.ajax({
+            "type": "post",
+            "dataType": "text",
+            "contentType": "application/json",
+            "async": false,
+            "url": "/thesis/delete",
+            "data": JSON.stringify(ids),
+            "success": function (data) {
+                console.log("success")
+                window.location.href = "/thesis"
+            },
+            "error": function (data) {
+                console.log("failure")
+            }
+        })
+    }
 })
